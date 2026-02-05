8d1c7f6ea07a7ed6c91d4566143c74132980ec64
Luis Fernando Illapa <moxb@outlook.es>
2026-02-05 13:23:39 -0500

    db: add schema script

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+CREATE SCHEMA IF NOT EXISTS domainwatcher;
+
+create table domainwatcher.dw_registry(
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    domain varchar(128) NOT NULL,
+    origin varchar(128) NOT NULL,
+    registry_created_at timestamp NULL,
+    registry_updated_at timestamp NULL,
+    registry_expires_at timestamp NULL,
+    created_at timestamp DEFAULT now() NOT NULL,
+    updated_at timestamp NULL,
+    deleted_at timestamp NULL,
+    CONSTRAINT registry_pkey PRIMARY KEY (id)
+);
+
+create table domainwatcher.dw_watcher(
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    registry_id uuid NOT NULL,
+    mail_address varchar(128),
+    notification_enabled boolean NOT NULL DEFAULT TRUE,
+    created_at timestamp DEFAULT now() NOT NULL,
+    updated_at timestamp NULL,
+    deleted_at timestamp NULL,
+    CONSTRAINT watcher_ukey UNIQUE (mail_address, registry_id),
+    CONSTRAINT registry_id_fkey FOREIGN KEY (registry_id) REFERENCES domainwatcher.dw_registry(id),
+    CONSTRAINT watcher_pkey PRIMARY KEY (id)
+);
