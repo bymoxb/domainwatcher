a36cad2aeb537844a175da4d238f60e8eea3cc84
Luis Fernando Illapa <moxb@outlook.es>
2026-02-05 19:11:50 -0500

    db: update columns constrains

--- db/schema.sql
+++ db/schema.sql
@@ -3,9 +3,10 @@ create table dw_registry(
     id uuid DEFAULT gen_random_uuid() NOT NULL,
     domain varchar(128) NOT NULL,
     origin varchar(128) NOT NULL,
-    registry_created_at timestamp NULL,
+    registry_created_at timestamp NOT NULL,
     registry_updated_at timestamp NULL,
-    registry_expires_at timestamp NULL,
+    registry_expires_at timestamp NOT NULL,
+    --
     created_at timestamp DEFAULT now() NOT NULL,
     updated_at timestamp NULL,
     deleted_at timestamp NULL,
@@ -17,10 +18,11 @@ create table dw_watcher(
     registry_id uuid NOT NULL,
     mail_address varchar(128),
     notification_enabled boolean NOT NULL DEFAULT TRUE,
+    --
     created_at timestamp DEFAULT now() NOT NULL,
     updated_at timestamp NULL,
     deleted_at timestamp NULL,
     CONSTRAINT watcher_ukey UNIQUE (mail_address, registry_id),
-    CONSTRAINT registry_id_fkey FOREIGN KEY (registry_id) REFERENCES domainwatcher.dw_registry(id),
+    CONSTRAINT registry_id_fkey FOREIGN KEY (registry_id) REFERENCES dw_registry(id),
     CONSTRAINT watcher_pkey PRIMARY KEY (id)
 );
